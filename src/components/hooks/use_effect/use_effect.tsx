c38f8a977c5a3171db7ba84f6a2a2c2247021986
TimalkaK <timalkamk@gmail.com>
2023-09-07 17:32:30 +0100

    feat: Implemented useEffect hook

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,35 @@
 // make an api call using useEffect
+import { useEffect, useState } from "react";
 
 export const APICall = () => {
 
-    // Your code here!
+    const [todo, setTodo] = useState(null);
+
+    useEffect(() => {
+        console.log("The todo is:", todo);
+
+        const fetchTodo = async () =>{
+            try{
+                const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+                const text = await response.json();
+                setTodo(text);
+            }catch(error){
+                console.log(error);
+            }
+        }
+
+        fetchTodo();
+
+        return () => {
+            console.log("Clean me up");
+        } 
+    },[todo]);
+
 
     return (
         <>
             <h2>useEffect</h2>
-            <p>Your todo here!</p>
+            <p>{JSON.stringify(todo)}</p>
         </>
     )
 }
